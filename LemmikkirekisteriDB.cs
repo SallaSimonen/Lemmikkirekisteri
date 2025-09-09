df381fb94bd1acb94a27517988a8a441220f4350
SallaSimonen <p38362@edu.sasky.fi>
2025-09-09 19:51:22 +0300

    Modified Program.cs

--- LemmikkirekisteriDB.cs
+++ LemmikkirekisteriDB.cs
@@ -14,12 +14,24 @@ public class LemmikkirekisteriDB
 
         // Luodaan taulu Omistajat sarakkeet id, nimi, puhelinnumero.
         var command1 = connection.CreateCommand();
-        command1.CommandText = "CREATE TABLE Omistajat (id INTEGER PRIMARY KEY, nimi TEXT, puhelinnumero VARCHAR(20))";
+        command1.CommandText = @"CREATE TABLE Omistajat (
+            id INTEGER PRIMARY KEY,
+            nimi TEXT,
+            puhelinnumero VARCHAR(20)
+            )";
+
         command1.ExecuteNonQuery();
 
         // Luodaan taulu Lemmikit sarakkeet id, omistaja_id, nimi, laji.
         var command2 = connection.CreateCommand();
-        command2.CommandText = "CREATE TABLE Lemmikit (id INTEGER PRIMARY KEY, omistaja_id INTEGER FOREIGN KEY (omistaja_id) (Omistajat(id)), nimi TEXT, laji TEXT)";
+        command2.CommandText = @"CREATE TABLE Lemmikit (
+            id INTEGER PRIMARY KEY,
+            omistaja_id INTEGER,
+            nimi TEXT, 
+            laji TEXT,
+            FOREIGN KEY (omistaja_id) REFERENCES Omistajat(id)
+            )";
+
         command2.ExecuteNonQuery();
 
         // Suljetaan yhteys.
--- Program.cs
+++ Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        LemmikkirekisteriDB rekisteri = new LemmikkirekisteriDB();
+
+        Console.WriteLine("Tervetuloa Lemmikkirekisteriin!");
+        Thread.Sleep(1000);
+        Console.Clear();
+
+        while (true)
+        {
+            Console.WriteLine("Lisää omistaja: 1\nLisää lemmikki: 2\nPäivitä puhelinnumero: 3\nEtsi puhelinnumero: 4\nLopeta:  0");
+            string? vastaus = Console.ReadLine();
+
+            switch (vastaus)
+            {
+                case "1":
+                    Console.Clear();
+                    rekisteri.LisaaOmistaja();
+                    break;
+
+                case "2":
+                    Console.Clear();
+                    rekisteri.LisaaLemmikki();
+                    break;
+
+                case "3":
+                    Console.Clear();
+                    rekisteri.PaivitaPuhelinnumero();
+                    break;
+
+                case "4":
+                    Console.Clear();
+                    rekisteri.EtsiPuhelinnumero();
+                    break;
+
+                case "0":
+                    Console.Clear();
+                    Console.WriteLine("Kiitos, kun käytit Lemmikkirekisteriä!");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    return;
+                    
+                default:
+                    Console.WriteLine("Anna 1, 2, 3, 4 tai 0");
+                    break;
+            }
+
+        }
     }
 }
